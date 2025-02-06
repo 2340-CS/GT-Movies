21eb89fef0dc254b1e50f58af0800574bb2af068
Nima Mollaei <nmollaei3@gatech.edu>
2025-02-06 16:16:04 -0500

    Added movie model

--- gtmovies/moviesapp/models.py
+++ gtmovies/moviesapp/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Review(models.Model):
-    id = models.AutoField(primary_key=True)
-    comment = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __str__(self):
-        return str(self.id) + ' - ' + self.movie.name
-
 class Movie(models.Model):
     movie_name = models.CharField(max_length=100)
     description = models.TextField()
@@ -18,3 +9,12 @@ class Movie(models.Model):
     RatingType = models.TextChoices("RatingType", "G PG PG-13 R")
     rating = models.CharField(choices=RatingType)
     genre = models.CharField(max_length=30)
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.movie.name
