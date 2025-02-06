3c00104ae435531f08e6fcfbafa7b0181c8b3966
Nima Mollaei <nimamollaei@lawn-10-91-35-63.lawn.gatech.edu>
2025-02-06 16:09:09 -0500

    Added movie model

--- gtmovies/moviesapp/models.py
+++ gtmovies/moviesapp/models.py
@@ -9,3 +9,12 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
+
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.DurationField()
+    price = models.FloatField()
+    RatingType = models.TextChoices("RatingType", "G PG PG-13 R")
+    rating = models.CharField(choices=RatingType)
+    genre = models.CharField(max_length=30)
