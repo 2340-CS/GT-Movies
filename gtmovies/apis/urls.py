352d3518f1f5b2f05190f6276824986b794771e5
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2025-02-07 20:12:02 -0500

    All user api requests work... Still needs unit testing though

--- gtmovies/apis/urls.py
+++ gtmovies/apis/urls.py
@@ -4,5 +4,7 @@ from rest_framework import routers
 
 urlpatterns = [
     path('user-list', views.ListUsers.as_view(), name="ListUsers"),
-    path('register-user', views.RegisterUser.as_view(), name="RegisterUser")
+    path('register-user', views.RegisterUser.as_view(), name="RegisterUser"),
+    path('login-user', views.LoginUser.as_view(), name="LoginUser"),
+    path('logout-user', views.LogoutUser.as_view(), name='LogoutUser')
 ]
--- gtmovies/apis/views.py
+++ gtmovies/apis/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse;
 from rest_framework import views, permissions
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from . import serializers
 
 
@@ -13,11 +15,8 @@ class ListUsers(views.APIView):
     def get(self, request):
         all_users = User.objects.all()
         print(all_users)
-        serializer = serializers.UserSerializer(data=all_users, many=True)
-        if (serializer.is_valid()):
-            return Response(serializer.data)
-        else:
-            return Response("Error retrieving user list.")
+        serializer = serializers.UserSerializer(all_users, many=True)
+        return Response(serializer.data)
         
 class ListMovies(views.APIView): 
 
@@ -41,18 +40,62 @@ class RegisterUser(views.APIView):
         user_serializer = serializers.UserSerializer(data=request.data)
 
         if (user_serializer.is_valid()):
-            username = request.query_params.get("username")
-            if username not in User.objects.all().filter(username=username):
-                user_serializer.save()
-                return Response(user_serializer.validated_data)
-        else:
-            return Response('User not created sucessfully')
+
+            username = user_serializer.validated_data['username']
+            password = user_serializer.validated_data['password']
+            email = user_serializer.validated_data['email']
+            
+            try:
+                User.objects.get(username=username)
+                return Response({
+                    "error": "The username inputted is already in use"
+                }, status=404)
+            except User.DoesNotExist:
+                User.objects.create_user(username=username, password=password, email=email)
+                return Response(user_serializer.validated_data, status=200)
+            
+        return Response({
+                    "error": "Bad data"
+                }, status=404)
         
 class LoginUser(views.APIView): 
 
     permission_classes = [permissions.AllowAny]
 
-    
+    # Authenticates User
+    def post(self, request): 
+
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not (username and password): 
+            return Response({"error": "Username and password are required"}, status=400)
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None: 
+            login(request, user)
+            return Response({"User sucessfully logged in"}, status="200")
+        
+        return Response({"Failed to login"}, status="404")
+
+
+class LogoutUser(views.APIView): 
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Logs out user
+    def get(self, request): 
+        if (request.user is not None and request.user.is_authenticated):
+            logout(request)
+            return Response({
+                "message": "Successfully logged out", 
+            }, status=200)
+        else: 
+            return Response({
+                "error": "Failed to log out", 
+            }, status=400)
+
 
 
 
