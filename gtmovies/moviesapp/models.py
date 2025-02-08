d0f55c77be1d0cc3d0572d304dbe2b97b4056782
JeffZhou42 <purpleshep42yee@gmail.com>
2025-02-08 18:18:05 -0500

    created serializer for reviews

--- gtmovies/moviesapp/models.py
+++ gtmovies/moviesapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rest_framework import serializers
 
 class Movie(models.Model):
     movie_name = models.CharField(max_length=100)
@@ -18,3 +19,13 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
+    
+class ReviewSerializer(serializers.Serializer):
+    id = serializers.AutoField()
+    comment = serializers.TextField()
+    date = serializers.DateTimeField()
+    movie = serializers.ForeignKey(Movie)
+    user = serializers.ForeignKey(User)
+
+    def create(self, validated_data):
+        return Review(**validated_data)
