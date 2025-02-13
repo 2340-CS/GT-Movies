b5239c8de539fd79654ea1842affd1c11d83b108
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2025-02-13 16:10:51 -0500

    fix some merging issues

--- gtmovies/moviesshow/apps.py
+++ gtmovies/moviesshow/apps.py
@@ -3,8 +3,4 @@ from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-<<<<<<<< HEAD:gtmovies/moviesshow/apps.py
-    name = 'moviesshow'
-========
-    name = 'home'
->>>>>>>> main:gtmovies/home/apps.py
+    name = 'moviesapp'
